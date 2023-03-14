62cbe51c7e17a6ef0c6cf03f57db6cfbfd897641
YouCanBeGod <dallasta.amos@gmail.com>
2023-03-13 21:20:26 -0300

    Updating UserController

--- src/main/java/dio/myfirstwebapi/controller/UserController.java
+++ src/main/java/dio/myfirstwebapi/controller/UserController.java
@@ -3,29 +3,32 @@ package dio.myfirstwebapi.controller;
 import dio.myfirstwebapi.model.User;
 import dio.myfirstwebapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository repository;
-    @GetMapping("/users")
+    @GetMapping
     public List<User>getUsers() {
         return repository.findAll();
     }
 
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public User getOne(@PathVariable("username") String username) {
         return repository.findByUsername(username);
     }
 
-    @DeleteMapping("/users/{id}")
-    public void delete(@PathVariable("id") Integer id) {
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
         repository.deleteById(id);
+    }
+
+    @PostMapping
+    public void postUser(@RequestBody User user) {
+        repository.save(user);
     }
 }
